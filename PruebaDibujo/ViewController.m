88b699cd239f716186765230ab35959782d1ea38
Ma.Elena Melón Jareda <mmelon@MaElena-Melon-Jaredas-MacBook-Pro.local>
2013-09-08 20:54:09 -0500

    Initial Commit

---
+++ PruebaDibujo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PruebaDibujo
+//
+//  Created by Ma.Elena Melón Jareda on 9/8/13.
+//  Copyright (c) 2013 Ma.Elena Melón Jareda App Studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
